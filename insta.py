d5c915eab307b49eb463da615ed80e572131082f
Biswajit Das <biswajitbangalore@gmail.com>
2020-07-05 12:23:59 +0100

    urllib fix

--- insta.py
+++ insta.py
@@ -28,7 +28,7 @@ def DownloadSingleFile(fileURL):
 	metaTag = soup.find_all('meta', {'property':'og:image'})
 	imgURL = metaTag[0]['content']
 	fileName = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S") + '.jpg'
-	urllib.urlretrieve(imgURL, fileName)
+	urllib.request.urlretrieve(imgURL, fileName)
 	print('Done. Image saved to disk as ' + fileName)
 
 
